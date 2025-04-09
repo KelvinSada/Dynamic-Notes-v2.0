a36a7d3ad99b14eefd092b2f547dd575acfd0687
Novoh .K. Sada <kelvinsada23@gmail.com>
2025-04-09 23:54:45 +0100

    added the menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Header from './Header'
 import Home from './Home'
+import Menu from './Menu'
 
 function App() {
 
   return (
     <>
-    <main className='min-h-screen bg-[#f4f4f4]'>
+    <main className='relative min-h-screen bg-[#f4f4f4]'>
       <Header/>
       <Home/>
+      <div className='absolute bottom-5 w-full'>
+        <Menu/>
+      </div>
     </main>
     </>
   )
---
+++ src/components/Menu.tsx
@@ -0,0 +1,32 @@
+import { IoIosAddCircle } from "react-icons/io";
+import { FaTableList } from "react-icons/fa6";
+import { IoMdSettings } from "react-icons/io";
+
+const Menu = () => {
+  return (
+    // <div className="w-fit mx-auto">
+    //   <ul className="flex bg-gray-200 border-[1px] text-[30px] border-gray-300 rounded-[30px] justify-between items-center gap-3 px-4 py-1">
+    //     <li className="hover:text-blue-500 hover:bg-gray-300 p-1 rounded-[20px]"><FaTableList /></li>
+    //     <li className="hover:text-blue-500 hover:bg-gray-300 p-1 rounded-[20px]"><IoIosAddCircle /></li>
+    //     {/* <li className="text-blue-500 bg-blue-200 p-2 rounded-[30px]"><IoIosAddCircle /></li> */}
+    //     <li className="hover:text-blue-500 hover:bg-gray-300 p-1 rounded-[20px]"><IoMdSettings /></li>
+    //   </ul>
+    // </div>
+
+    <div className="w-fit mx-auto">
+      <ul className="flex bg-gray-100 border border-gray-200 text-2xl rounded-full shadow-sm overflow-hidden">
+        <li className="px-4 py-2 text-gray-600 hover:bg-blue-100 transition-colors cursor-pointer">
+          <FaTableList />
+        </li>
+       <li className="px-4 py-2 text-gray-600 hover:bg-blue-100 transition-colors cursor-pointer">
+         <IoIosAddCircle />
+       </li>
+       <li className="px-4 py-2 text-gray-600 hover:bg-blue-100 transition-colors cursor-pointer">
+          <IoMdSettings />
+       </li>
+     </ul>
+    </div>
+  )
+}
+
+export default Menu
