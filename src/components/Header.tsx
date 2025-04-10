780e34e113007c89327c1af0080e22f66a7fbfac
Novoh .K. Sada <kelvinsada23@gmail.com>
2025-04-10 11:55:59 +0100

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@
 const Header = () => {
   return (
     <header className="w-full bg-[#F8F8F8] flex flex-col items-center">
-      <div className="flex justify-between items-center w-[92%] py-2 ">
-        <p>Toggle</p>
+      <div className="flex justify-center items-center w-[92%] py-2 ">
+        {/* <p>Toggle</p> */}
         <img src="logo-dark-transparent.png"
         className=" block w-[45%] md:w-[18%]" alt="Dynamic notes Logo"/>
-        <p>Search</p>
+        {/* <p>Search</p> */}
       </div>
     </header>
   )
