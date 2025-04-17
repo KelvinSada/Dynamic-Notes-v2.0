852cd06dbc2ba9c99cc3b9f4772c19f6a77bd2f5
Novoh .K. Sada <kelvinsada23@gmail.com>
2025-04-17 11:14:02 +0100

    .

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -27,9 +27,9 @@ const NoteItem = ({values,pickedNote,setpickedNote}:Values) => {
   return (
     <section className=" flex flex-col gap-3 border-1 border-gray-300 hover:border-blue-300 rounded-[10px] p-3 bg-[#F8F8F8]">
       <div className="flex items-center justify-between">
-        <h2 onClick={handleViewNotes} className="text-2xl bg-red-300 text-gray-800 font-700">{values.title}</h2>
+        <h2 onClick={handleViewNotes} className="text-2xl text-gray-800 font-700">{values.title}</h2>
         <div className="relative text-lg hover:bg-gray-300 py-1 hover:rounded-[10px] text-black">
-          <div>{values.id}</div>
+          {/* <div>{values.id}</div> */}
           <button onClick={()=>ToggleClick(values.id)}>
             <CiMenuKebab />
           </button>
