ec9f95a5db24e2d051f1c763aa33b6295f4ad47a
Novoh .K. Sada <kelvinsada23@gmail.com>
2025-06-08 18:13:05 +0100

    .

--- src/components/SavedItems.tsx
+++ src/components/SavedItems.tsx
@@ -11,7 +11,7 @@ const SavedItems = () => {
     <div className=" w-[92%] mx-auto">
       <h1 className="text-3xl px-3 py-3 text-gray-700 ">Saved Items</h1>
 
-      {savedArray.length > 0? <div className="grid bg-rose-400 grid-col-1 sm:grid-cols-2 gap-2 ">
+      {savedArray.length > 0? <div className="pb-40 grid grid-col-1 sm:grid-cols-2 gap-2 ">
         {savedArray.map((items)=>{
           return(
             <NoteItem pickedNote={pickedNote} setpickedNote={setPickedNote} key={items.id} values={items}/>
